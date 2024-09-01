0c6a50c0c83330e66430b8587029dc02f5143731
Victor Sousa <victor.henriqueoff@gmail.com>
2024-09-01 02:07:23 -0300

    Clip Controllers: Get Clips

---
+++ src/controllers/clip-controllers/get-clips-controller.ts
@@ -0,0 +1,32 @@
+import { Request, Response } from "express";
+import { StreamerRepositoryDatabase } from "../../repositories/database/streamer-repository-database";
+import moment from "moment";
+import { GetClipsService } from "../../services/clips-services/get-clips-service";
+import { GetClipsHandleErrors } from "./errors/get-clips-handle-errors";
+
+export async function GetClipController(request: Request, response: Response) {
+
+	const { amount, startedAt } = request.query;
+	const tokenTwitch = request.token;
+
+	const querys = {
+		amount: amount ? amount.toString() : "5",
+		startedAt: startedAt ? startedAt.toString() : moment().subtract(1, "days").format("YYYY-MM-DD"),
+		tokenTwitch
+	};
+    
+	try {
+
+		const streamerRepositoryDatabase = new StreamerRepositoryDatabase();  
+		const getClipsService = new GetClipsService(streamerRepositoryDatabase);
+
+		await getClipsService.execute(querys);
+
+		return response.json({
+			message: "Clipes encontrados com sucesso!",
+			success: true
+		}).status(200);
+	} catch (error) {
+		GetClipsHandleErrors(response, error);
+	}
+}
---
+++ src/services/clips-services/get-clips-service.ts
@@ -0,0 +1,50 @@
+import { Clip } from "@prisma/client";
+import { StreamerRepositoryInterface } from "../../repositories/interfaces/streamer-repository-interface";
+import moment from "moment";
+import { GetClipsInTwitchApiClient } from "../../http/api-client-http";
+
+interface GetClipsServiceInterface {
+    amount: string;
+    startedAt: string;
+    tokenTwitch: string;
+}
+
+interface ClipInterface {
+    streamer_id: string;
+    streamer_name: string;
+    clips: Clip[];
+}
+
+export class GetClipsService{
+	constructor(
+        private StreamerRepositoryDatabase: StreamerRepositoryInterface
+	) {}
+
+	async execute({amount, startedAt, tokenTwitch}:GetClipsServiceInterface){
+
+		const streamers = await this.StreamerRepositoryDatabase.findManyStreamer();
+
+		const clips: ClipInterface[] = [];
+
+		const formattedDate = moment(startedAt, "YYYY-MM-DD").toISOString();
+
+		const promises = streamers.map(async (streamer) => {
+			const clipsStreamer = await GetClipsInTwitchApiClient(
+				streamer.id,
+				formattedDate,
+				amount,
+				tokenTwitch
+			);
+
+			clips.push({
+				streamer_id: streamer.id,
+				streamer_name: streamer.login,
+				clips: clipsStreamer
+			});
+		});
+
+		await Promise.all(promises);
+
+		return clips;
+	}
+}
